23b0c788a60168cfa316617071970381d478b755
Neil-Kasson <Neil-Kasson@users.noreply.github.com>
2020-12-29 14:50:49 -0500

    More backend stuff

--- src/main/java/com/tts/loving_spoonful/controller/AuthorizationController.java
+++ src/main/java/com/tts/loving_spoonful/controller/AuthorizationController.java
@@ -1,5 +1,30 @@
 package com.tts.loving_spoonful.controller;
 
+import com.tts.loving_spoonful.model.User;
+import com.tts.loving_spoonful.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizationController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping(value = "/signup")
+	public String registration(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "registration";
+	}
 
 }
